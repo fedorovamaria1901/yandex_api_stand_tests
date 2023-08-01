48a09794304473699df355c00d103d95f4736cea
lovelifealot <peredat4ik@gmail.com>
2023-08-02 03:48:24 +0500

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://193c41aa-7efe-476e-918a-6355bd3aeb04.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
